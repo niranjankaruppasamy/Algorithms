d30e3407654b84553f9dcba9804a3bfb1a4b9665
niranjankaruppasamy <niranjan.karuppa.vc@startv.com>
2021-08-31 00:26:26 +0530

    added more properties and function for linked list

--- python/singly_linked_list.py
+++ python/singly_linked_list.py
@@ -15,6 +15,23 @@ class LinkedList:
         self.head = node
 
     def insert_at_end(self, val:Any):
+        node = self.head
+        while node:
+            if node.next is None:
+                node.next = Node(val, None)
+                break
+            node = node.next
+
+    @property
+    def get_length(self) -> int:
+        counter = 0
+        node = self.head
+        while node:
+            node = node.next
+            counter += 1
+        return counter
+
+    def insert_at(self, val: Any, index: int):
         ...
 
     def print(self):
@@ -31,4 +48,8 @@ class LinkedList:
 if __name__ == "__main__":
     ll = LinkedList()
     ll.insert_at_beginning(3)
+    ll.insert_at_beginning(10)
+    ll.insert_at_end(5)
+    ll.insert_at_end(65)
     ll.print()
+    print(ll.get_length)
