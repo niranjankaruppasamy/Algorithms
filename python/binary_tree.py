b45a75e55ada1bda7923854fe1be2f5fa68f5d9f
niranjankaruppasamy <niranjan.karuppa.vc@startv.com>
2021-12-05 23:40:22 +0530

    binary tree update

--- python/binary_tree.py
+++ python/binary_tree.py
@@ -1,18 +1,41 @@
 from typing import Any
 
 
-class Node:
-    def __init__(self, data: Any, left: Any = None, right: Any = None) -> None:
+class Tree:
+    def __init__(self, data: Any) -> None:
         self.data = data
-        self.left = left
-        self.right = right
-
-
-class BinaryTree:
-    def __init__(self) -> None:
-        self.root = None
+        self.left = None
+        self.right = None
 
     def insert(self, val: Any):
-        if self.root is None:
-            self.root = Node(val)
+        if self.data is None:
+            self.data = Tree(val)
             return
+        if val < self.data:
+            if self.left:
+                self.left.insert(val)
+            else:
+                self.left = Tree(val)
+        else:
+            if self.right:
+                self.right.insert(val)
+            else:
+                self.right = Tree(val)
+
+    def print_tree(self):
+        # pre order traversal
+        print(self.data)
+
+        if self.left:
+            self.left.print_tree()
+        if self.right:
+            self.right.print_tree()
+
+
+if __name__ == "__main__":
+    tree = Tree(50)
+    tree.insert(25)
+    tree.insert(30)
+    tree.insert(40)
+    tree.insert(80)
+    tree.print_tree()
